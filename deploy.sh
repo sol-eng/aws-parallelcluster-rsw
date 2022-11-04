28c35bb769d3b55132aa15a4df3ce7782ab639be
Michael Mayer <michael.mayer@rstudio.com>
2022-11-04 11:54:36 +0100

    add EFS

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-CLUSTERNAME="demo2"
+CLUSTERNAME="centos7"
 S3_BUCKETNAME="hpc-scripts1234"
 SECURITYGROUP_RSW="sg-0d432b9a4aeea0dd6"
 SUBNETID="subnet-9bbd91c1"
 REGION="eu-west-1"
 KEY="michael"
 CERT="/Users/michael/projects/aws/certs/michael.pem"
+EFS="172.31.33.253:/opt/apps"
 
 cat scripts/aliases.tmpl | sed "s#CERT#${CERT}#" > scripts/aliases.sh
-cat scripts/install-rsw.sh.tmpl | sed "s#S3_BUCKETNAME#${S3_BUCKETNAME}#g" > scripts/install-rsw.sh
-cat scripts/install-compute.sh.tmpl | sed "s#S3_BUCKETNAME#${S3_BUCKETNAME}#g" > scripts/install-compute.sh
+cat scripts/install-rsw.sh.tmpl | sed "s#S3_BUCKETNAME#${S3_BUCKETNAME}#g" | sed "s#EFS#${EFS}#" > scripts/install-rsw.sh
+cat scripts/install-compute.sh.tmpl | sed "s#S3_BUCKETNAME#${S3_BUCKETNAME}#g" | sed "s#EFS#${EFS}#" > scripts/install-compute.sh
 aws s3 cp scripts/ s3://${S3_BUCKETNAME} --recursive --exclude security-group.sh --exclude *.tmpl 
 
 cat config/cluster-config-wb.tmpl | \
