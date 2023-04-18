a644941f5812ee2a8e23b253a7e0e73cd0b909a7
Michael Mayer <michael.mayer@posit.co>
2023-04-18 20:53:44 +0200

    align uid and gid of rstudio

--- scripts/install-compute.sh
+++ scripts/install-compute.sh
@@ -12,8 +12,8 @@ apt-get install -y python3.8-venv
 apt-get install -y libfreetype6-dev libpng-dev libtiff5-dev
 
 # Add sample user 
-groupadd --system --gid 1001 rstudio
-useradd -s /bin/bash -m --system --gid rstudio --uid 1001 rstudio
+groupadd --system --gid 8787 rstudio
+useradd -s /bin/bash -m --system --gid rstudio --uid 8787 rstudio
 
 echo -e "rstudio\nrstudio" | passwd rstudio
 
