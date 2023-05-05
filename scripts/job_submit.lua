6e68a14e2dd9402bf23243c49aafb773711ca3d6
Michael Mayer <michael.mayer@posit.co>
2023-05-05 15:31:13 +0200

    fix job submit plugin to allow zero gpu request

--- scripts/job_submit.lua
+++ scripts/job_submit.lua
@@ -1,14 +1,12 @@
 --[[
 
-Simple job_submit plugin that redirects all jobs to the gpu partition if GPUs are requested 
+Simple job_submit plugin that redirects all jobs to the gpu partition if a non-zero amount of GPUs are requested 
 
 --]]
 
 function slurm_job_submit(job_desc, job_rec, part_list, modify_uid)
-	if string.match(job_desc['tres_per_job'],'gpu')  then
-		slurm.log_info("slurm_job_modify: job requested partition %s but seems to need GPUs, hence we redirect to partition gpu",
-				job_desc.partition)
-		job_desc.partition = "gpu"
+	if ( string.match(job_desc['tres_per_job'],'gpu') and not string.match(job_desc['tres_per_job'],'gpu.*:0') ) then
+			job_desc.partition = "gpu"
 	end
 
 	return slurm.SUCCESS
@@ -19,6 +17,6 @@ function slurm_job_modify(job_desc, job_rec, part_list, modify_uid)
    return slurm.SUCCESS
 end
 
-slurm.log_info("initialized")
+slurm.log_info("SLURM job submit plugin initialized")
 return slurm.SUCCESS
 
