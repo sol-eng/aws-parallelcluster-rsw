406e11604e7d8c339fec62b31aa0a01991b13b6c
Michael Mayer <michael.mayer@posit.co>
2023-08-30 15:05:21 +0200

    fix perm issue with vscode extension

--- scripts/install-rsw.sh
+++ scripts/install-rsw.sh
@@ -93,7 +93,7 @@ setfacl -R --set-file=/tmp/acl /data/renv
 groupadd --system --gid 900 rstudio-server
 useradd -s /bin/bash -m --system --gid rstudio-server --uid 900 rstudio-server
 RSWB_VER=PWB_VER
-curl -O https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-workbench-${RSWB_VER}-amd64.deb 
+curl -O https://s3.amazonaws.com/rstudio-ide-build/server/focal/amd64/rstudio-workbench-${RSWB_VER}-amd64.deb 
 gdebi -n rstudio-workbench-${RSWB_VER}-amd64.deb
 rm -f rstudio-workbench-${RSWB_VER}-amd64.deb
 
@@ -335,6 +335,8 @@ for extension in quarto.quarto \
 do
    /usr/lib/rstudio-server/bin/code-server/bin/code-server --extensions-dir=/opt/rstudio/code-server --install-extension $extension
 done
+
+chmod a+rx /opt/rstudio/code-server
 
 rm -f /etc/rstudio
 
